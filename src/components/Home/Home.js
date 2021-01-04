19d50044af9c799469930abf12238b1c919295d4
Lance Hamilton <lance.hamilton24@gmail.com>
2021-01-04 10:31:04 -0600

    added BeWell logo to the home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './Home.scss';
+import logo from '../../images/bewellLogo.jpg'
 
 class Home extends React.Component {
 
   render() {
-    return <h1>Home Page</h1>;
+    return (
+      <div classname="home-page">
+        <div classname="welcome-message">
+          <h1>Welcome to the BeWell Room!</h1>;
+    </div>
+        <div classname="logo">
+          <img src={logo}></img>
+        </div>
+      </div>
+    )
   }
-
 }
 
 export default Home;
