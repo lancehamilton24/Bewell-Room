3e3ec2509ca2067dee110c99090fe5fd20232288
Lance Hamilton <lance.hamilton24@gmail.com>
2021-01-04 11:15:47 -0600

    Updated home page styling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import logo from '../../images/bewellLogo.jpg';
 import './Home.scss';
-import logo from '../../images/bewellLogo.jpg'
 
 class Home extends React.Component {
 
   render() {
     return (
-      <div classname="home-page">
-        <div classname="welcome-message">
-          <h1>Welcome to the BeWell Room!</h1>;
-    </div>
-        <div classname="logo">
+      <div className="home-page">
+        <div className="welcome-message">
+          <h1>Welcome to the BeWell Room!</h1>
+        </div>
+        <div className="logo">
           <img src={logo}></img>
         </div>
       </div>
